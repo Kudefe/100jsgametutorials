757d7489bb086ded0be25f087deb6c65cc478aed
kudefe <kudefeg@gmail.com>
2018-01-16 22:15:12 -0300

    Flappy Bird

--- Flappy Birds/index.js
+++ Flappy Birds/index.js
@@ -1,6 +1,6 @@
 ;(window.onload = function () {
 
-    const cvs = document.getElementById('canvas')
+let cvs = document.getElementById('canvas')
 let context = cvs.getContext('2d')
 cvs.style.border = '1px solid'
 
@@ -12,17 +12,92 @@ let fg = new Image ()
 let pipeNorth = new Image ()
 let pipeSouth = new Image ()
 
-bird.src = './bird.png'
-bg.src = './bg.png'
-fg.src = './fg.png'
-pipeNorth.src = './pipen.png'
-pipeSouth.src = './pipes.png'
+bird.src = './img/bird.png'
+bg.src = './img/bg.png'
+fg.src = './img/fg.png'
+pipeNorth.src = './img/pipeNorth.png'
+pipeSouth.src = './img/pipeSouth.png'
+
+const gap = 90
+let constant
+
+//bird position
+let bx = 10
+let by = 150
+
+const gravity = 1.5
+
+let score = 0
+
+//audio files
+const fly = new Audio()
+const scor = new Audio()
+
+fly.src = './sounds/fly.mp3'
+scor.src = './sounds/score.mp3'
+
+//key down
+document.addEventListener('keydown', moveUp)
+
+function moveUp () {
+    by -= 25
+    fly.play()
+}
+
+//pipe coordenates
+let pipe = []
+
+pipe[0] = {
+    x: cvs.width,
+    y: 0
+}
 
 // draw images
 
 function draw () {
-    context.drawImage(bg, 0, 0, 288, 512)    
+
+    context.drawImage(bg, 0, 0)
+
+    for (let i = 0; i < pipe.length; i++) {
+
+        constant = pipeNorth.height + gap
+        context.drawImage(pipeNorth, pipe[i].x, pipe[i].y)
+        context.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant)
+
+        pipe[i].x--
+
+        if (pipe[i].x === 125) {
+            pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random()*pipeNorth.height) - pipeNorth.height
+            })
+        }
+        if( bx + bird.width >= pipe[i].x && bx <= pipe[i].x + pipeNorth.width && (by <= pipe[i].y + pipeNorth.height || by+bird.height >= pipe[i].y+constant) || by + bird.height >=  cvs.height - fg.height){
+            location.reload() // reload the page
+        }
+        if (pipe[i].x === 5) {
+            score++
+            scor.play()
+        }
+    }
+
+    context.drawImage(fg, 0, cvs.height - fg.height)
+    context.drawImage(bird, bx, by)
+
+    //bird gravity 
+    by += gravity
+
+    context.fillStyle='#000'
+    context.font='20px sans-serif'
+    context.fillText('Score : ' + score, 10, cvs.height - 20)
+    
+    
+    
+
+    requestAnimationFrame(draw)
+    
 }
 
 draw();
+
 })()
